80c9820ab5adb152960035e1c721d887c0e2e3ab
Martijn vE <martijnvaneeten@hotmail.com>
2018-02-08 20:29:36 +0100

    Module 13 changes

---
+++ src/vendor.js
@@ -0,0 +1,10 @@
+
+/*This file contains reference to the vendor libraries
+we're using in this project. This is used by webpack
+in the production build only.
+
+*/
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
